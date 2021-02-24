fbb64994c69749d9ad323ed34f288de6727033a4
sivakiran <sivapolireddy2212@gmail.com>
2021-02-24 19:09:56 +0530

    what happens now

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,24 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-const input = document.querySelector('input')
-const output = document.querySelector('p').document.querySelector('form').addEventListener('submit',(e)=> {
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const  area= input.value
-    fetch('/weather?address='+ area).then((responce) => {
-        responce.json().then((store) => {
-            if(store.error) {
-                console.log("not working")
-            }
-            else {
-                output.textContent=store.forecast
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         })
     })
-
 })
